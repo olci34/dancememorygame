6e7dba628b6ad6c20f3cdab4fb8d4a2b4df0dda0
github username <github email address>
2021-05-07 10:55:12 -0400

    defines #constructor method for Game class

---
+++ frontend/game.js
@@ -0,0 +1,9 @@
+class Game {
+    
+    constructor(card_number, click_number, score) {
+        this.card_number = card_number
+        this.click_number = click_number
+        this.score = score
+    }
+
+}
