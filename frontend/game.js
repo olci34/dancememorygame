eceef6fd5d4a0e5008cd0f5d8a7c2fb89ca97446
github username <github email address>
2021-05-07 10:58:59 -0400

    defines #calculateScore method for final score

--- frontend/game.js
+++ frontend/game.js
@@ -1,9 +1,13 @@
 class Game {
-    
+
     constructor(card_number, click_number, score) {
         this.card_number = card_number
         this.click_number = click_number
         this.score = score
+    }
+
+    calculateScore() {
+        (this.card_number * 100000) / this.click_number
     }
 
 }
