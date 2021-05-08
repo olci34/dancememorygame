49b591ab2a8ff1c2f682d824fb703d9d10f6a666
github username <github email address>
2021-05-07 22:48:42 -0400

    adds #show and #update methods to GamesController

--- backend/app/controllers/games_controller.rb
+++ backend/app/controllers/games_controller.rb
@@ -5,7 +5,19 @@ class GamesController < ApplicationController
         render json: games
     end
 
-    def create
-        binding.pry
+    def show
+        game = Game.find_by(id: params[:id])
+        render json: game
+    end
+
+    def update
+        game = Game.find_by(id: params[:id])
+        game.update(game_params)
+    end
+
+    private
+
+    def game_params
+        params.require(:game).permit(:click_number, :score)
     end
 end
