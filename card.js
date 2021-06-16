3a8aa8157c716f06d2152e414e096653d55091b0
github username <github email address>
2021-06-15 21:08:51 -0400

    creates game

---
+++ card.js
@@ -0,0 +1,122 @@
+let comparedCards = []
+let allCards = []
+let clicks = 0
+const cards = [
+                {sticker:'https://media.giphy.com/media/SUtvUAbKeBXiVdqCMB/giphy.gif', matchID: 1}, 
+                {sticker:'https://media.giphy.com/media/47GcWwJdOydNTuufsp/giphy.gif', matchID: 2},
+                {sticker:'https://media.giphy.com/media/TeBpzQZRaBIC4/giphy.gif', matchID: 3},
+                {sticker:'https://media.giphy.com/media/wn8rVP7qC8TNC/giphy.gif', matchID: 4},
+                {sticker:'https://media.giphy.com/media/p3BhK7nNlB9AMJrtYh/giphy.gif', matchID: 5},
+                {sticker:'https://media.giphy.com/media/5fBH6zrcIiOs65zXCtG/giphy.gif', matchID: 6},
+                {sticker:'https://media.giphy.com/media/dQpqkxXyPvb2iImius/giphy.gif', matchID: 7},
+                {sticker:'https://media.giphy.com/media/BMzLC8TYr7489CAjPM/giphy.gif', matchID: 8}
+            ]
+
+const cardBoard = document.querySelector('div.card-board')
+
+class Card {
+    
+    constructor(sticker, matchID, uniq) {
+        this.sticker = sticker
+        this.matchID = matchID
+        this.uniq = uniq // to find and flip unmatched cards when 3rd one clicked
+        this.faceUp = false
+    }
+
+    appendCard() {
+        const flipCard = document.createElement('div')
+        flipCard.className = 'flip-card'
+        const flipCardInner = document.createElement('div')
+        flipCardInner.className = 'flip-card-outer'
+        const frontDiv = document.createElement('div')
+        frontDiv.className = 'flip-card-front'
+        const backDiv = document.createElement('div')
+        backDiv.className = 'flip-card-back'
+        flipCardInner.title = this.matchID
+        flipCardInner.id = this.uniq
+        backDiv.innerHTML = `<img src='${this.sticker}'>`
+
+        flipCardInner.append(frontDiv)
+        flipCardInner.append(backDiv)
+        flipCard.append(flipCardInner)
+        cardBoard.append(flipCard)
+        flipCardInner.addEventListener('click', this.cardListener.bind(this))  // BLOG POST bind(this)
+    }
+
+    cardListener(e) {
+        const clickBoard = document.getElementById('click-number')
+        clickBoard.innerText = `${++clicks}`
+        if (comparedCards.length === 0) {
+            comparedCards.push(this)
+            comparedCards[0].flipFaceUp(e.currentTarget)
+        } else if (comparedCards.length === 1) {
+            if (this.faceUp === false) { 
+                comparedCards.push(this)
+                comparedCards[1].flipFaceUp(e.currentTarget)
+                const matchResult = Card.matchCard(comparedCards[0], comparedCards[1])
+                if (matchResult) {
+                    comparedCards = []
+                    Game.changeScoreBoard()
+                    const matchedDOMCards = document.querySelectorAll(`[title='${e.currentTarget.title}']`)
+                    matchedDOMCards.forEach(function(DOMCard) {
+                        DOMCard.style.pointerEvents = 'none'
+                        DOMCard.children[1].style.backgroundColor = 'rgba(153, 205, 50, 0.267)'
+                    })
+                } 
+            } else if (this.faceUp === true) { // If clicked to a only faced up card
+                comparedCards[0].flipFaceDown(e.currentTarget)
+                comparedCards = []
+            }
+        } else if (comparedCards.length === 2) {
+            comparedCards.forEach(function(cardObj) {
+                cardObj.flipFaceDown(document.getElementById(`${cardObj.uniq}`))
+            })
+            comparedCards = [this]
+            comparedCards[0].flipFaceUp(e.currentTarget)
+        }
+        if (Game.victory()) {
+            Game.patchGame()
+        }
+    }
+
+    static matchCard(card1,card2) {
+        return card1.matchID === card2.matchID
+    }
+
+    flipFaceUp(target) {
+        target.className = 'flip-card-inner' // to flip visually
+        this.faceUp = true // to flip programmatically
+    }
+
+    flipFaceDown(target) {
+        target.className = 'flip-card-outer'
+        this.faceUp = false
+    }
+
+    static setCards(cardNumber) {
+        allCards = []
+        let randomCards = [...cards]
+
+        cardBoard.innerHTML = ''
+        const score = document.getElementById('score')
+        score.innerText = '0'
+        const clickNumber = document.getElementById('click-number')
+        clickNumber.innerText = '0'
+        const cardsNumber = document.getElementById('numberOfCards')
+        cardsNumber.value = ''
+    
+        for (let i = 0; i < cardNumber/2; i++) {
+            const randomCard = randomCards.splice(Math.floor(Math.random() * randomCards.length),1)[0];
+            const newCard = new Card(randomCard.sticker, randomCard.matchID)
+            const matchCard = new Card(randomCard.sticker, randomCard.matchID)
+            allCards.push(newCard,matchCard)
+        }
+    
+        const shuffledCards = shuffle(allCards)
+        
+        for (let i = 0; i < shuffledCards.length; i++) {
+            shuffledCards[i].uniq = i
+            shuffledCards[i].appendCard()
+        }
+    }
+}
---
+++ player.js
@@ -0,0 +1,60 @@
+class Player {
+
+    constructor(rank, name, latest_score) {
+        this.rank = rank
+        this.name = name
+        this.latest_score = latest_score
+    }
+    
+    static createPlayer(target) {
+        const playerName = target.children[2].value
+        const gameCardNumber = parseInt(target.children[6].value, 10)
+        const newPlayer = {player: {name: playerName, games_attributes: {"0": {card_number: gameCardNumber}}}} // game_attributes
+        const configPlayer = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json', "Accept": "application/json"},
+            body: JSON.stringify(newPlayer)
+        }
+        
+        fetch('http://localhost:3000/players', configPlayer)
+           .then(resp => resp.json())
+           .then(player => {
+               if (player.id) {
+                    const gameID = document.getElementById('gameID')
+                    const lastGame = player.games[player.games.length - 1]
+                    gameID.value = lastGame.id
+                    document.querySelector('.click-counter').append(gameID)
+                    const currentPlayer = new Player(player.rank, player.name, player.latest_score)
+                    currentPlayer.appendPlayer() 
+                    Card.setCards(lastGame.card_number)
+                    disableConfig(false)
+                } else {
+                   throw new Error(player.message) /// add alert DOM
+                }
+           }).catch(err => alert(err))
+    }
+
+    appendPlayer() {
+        const topPlayersList = document.querySelector('tbody.players')
+        const newRow = document.createElement('tr')
+        newRow.innerHTML = `<td>${this.rank}</td>
+                            <td>${this.name}</td>
+                            <td>${this.latest_score}</td>`
+        topPlayersList.appendChild(newRow)
+    }
+
+    static listTopFive() {
+        const top5 = document.querySelector('.players')
+        top5.innerHTML = ''
+        fetch('http://localhost:3000/players')
+        .then(resp => resp.json())
+        .then(players => {
+            let sortedPlayers = players.sort( (p1, p2) => p1.rank - p2.rank)
+            let top5 = sortedPlayers.slice(0,5)
+            top5.forEach(function(player) {
+                const newPlayer = new Player(player.rank, player.name, player.latest_score)
+                newPlayer.appendPlayer()
+            })
+        })
+    }
+}
